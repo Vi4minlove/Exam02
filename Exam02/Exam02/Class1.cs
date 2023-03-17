34d7d754cfbf5b2eb745db8c7b85175d48c83e47
tahuynhduc <tahduc03032000@gmail.com>
2023-03-17 21:28:24 +0700

    Exam2

---
+++ Exam02/Exam02/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam02
+{
+    public interface IStudent
+    {
+        int StudID { get; set; }
+        int StudAge { get; set; }
+        string StudClass { get; set; }
+        string StudName { get; set; }
+        string StudGender { get; set; }
+
+        float StudAvgMark
+        {
+            get;
+        }
+        void Print();
+    }
+    class Student : IStudent
+    {
+        public int StudID { get ; set; }
+        public int StudAge { get; set; }
+        public string StudClass { get; set; }
+        public string StudName { get; set; }
+        public string StudGender { get; set; }
+
+        private float _studAvgMark;
+        public float StudAvgMark { get { return _studAvgMark; } }
+
+        public void Print()
+        {
+            Console.WriteLine($"{StudID}");
+            Console.WriteLine($"{StudAge}");
+            Console.WriteLine($"{StudClass}");
+            Console.WriteLine($"{StudName}");
+            Console.WriteLine($"{StudGender}");
+            Console.WriteLine($"{StudAvgMark}");
+        }
+        private int[] MarkList = new int[3];
+        public int this[int index]
+        {
+            get
+            {
+                return MarkList[index];
+            }
+            set
+            {
+                MarkList[index] = value;
+            }
+        }
+        public void CalAvg()
+        {
+            var sum = 0;
+            foreach (int mark in MarkList) { sum += mark; }
+            _studAvgMark = sum/MarkList.Length;
+        }
+    }
+}
---
+++ Exam02/Exam02/Program.cs
@@ -0,0 +1,39 @@
+﻿using Exam02;
+using System.Collections;
+
+namespace exam02
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("     1. Insert new student...");
+            Console.WriteLine("     2. Dislay all the student list...");
+            Console.WriteLine("     3. Calculator avenrage mark...");
+            Console.WriteLine("     4. find student...");
+            Console.WriteLine("     5. Exit.");
+            Console.WriteLine("=======================================");
+
+
+            char ch;
+            Console.WriteLine("Nhap vao ky tu:");
+            ch = (char)Console.Read();
+            switch (ch)
+            {
+                case '1':
+                    Student student = new Student();
+
+                    student.StudID = 1;
+                    student.StudAge = 2;
+                    student.StudClass = "class";
+                    student.StudName = "StudName";
+                    student.StudGender = "StudGender";
+
+                    student.Print();
+                    break;
+            }
+        }
+    }
+}
